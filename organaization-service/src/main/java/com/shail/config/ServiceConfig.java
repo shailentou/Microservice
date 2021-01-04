a08ff8c72c553f34a837e11bec2a050dc33d09e4
shailentou <shailentou@gmail.com>
2021-01-04 09:56:48 +0530

    commit

---
+++ organaization-service/src/main/java/com/shail/config/ServiceConfig.java
@@ -0,0 +1,17 @@
+package com.shail.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ServiceConfig {
+	 
+	
+	
+	@Value("${example.property}")
+	  private String exampleProperty;
+
+	  public String getExampleProperty(){
+	    return exampleProperty;
+	  }
+}

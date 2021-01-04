157578e23926da21992a9e351ee3fa587a231d44
shailentou <32923113+shailentou@users.noreply.github.com>
2021-01-04 08:23:39 +0530

    Update OrganaizationServiceApplication.java

--- organaization-service/src/main/java/com/shail/config/OrganaizationServiceApplication.java
+++ organaization-service/src/main/java/com/shail/config/OrganaizationServiceApplication.java
@@ -1,6 +1,6 @@
 package com.shail.config;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication; 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
